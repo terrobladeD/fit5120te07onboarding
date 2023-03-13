d66ff4bacecc6c22c44b57dce79ae80760e8b712
ZihengZhao101 <127199521+ZihengZhao101@users.noreply.github.com>
2023-03-13 18:45:07 +0800

    Update base.py

--- backend/base.py
+++ backend/base.py
@@ -17,15 +17,15 @@ collection_data= pd.read_csv('clean_data.csv',header=0)
 @api.route('/profile',methods=["POST"])
 def submit_input_value():
     input_value = request.form['inputValue']
-    print(input_value)
-    index = collection_data[collection_data['address'].str.contains(r'Albert Street')].index.tolist()[0]
+    # print(input_value)
+    index = collection_data[collection_data['address'].str.contains(input_value)].index.tolist()[0]
     day = collection_data['collectionday'][index]
     nextwaste = collection_data['nextwaste'][index]
-    nextrecycle = collection_data['nextwaste'][index]
-    nextgreen = collection_data['nextwaste'][index]
+    nextrecycle = collection_data['nextrecycle'][index]
+    nextgreen = collection_data['nextgreen'][index]
     response_data = {'day': day, 'landfilldate':nextgreen,'recycledate':nextrecycle,'fooddate':nextwaste}
-    print(response_data)
+    # print(response_data)
     test = input_value.upper()
     response_test = {'response': test,'response1': test}
-    print(response_test)
+    # print(response_test)
     return jsonify(response_data)
