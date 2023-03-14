4f2a5475ed0886eec5c13f59e160ac6b21a31d70
terrobladeD <terrobladediao@gmail.com>
2023-03-14 18:55:19 +1100

    onborading finish

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,22 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 
 api = Flask(__name__)
-
-# @api.route('/profile',methods=["POST", "GET"])
-# def my_profile():
-#     response_body = {
-#         "name": "Nagato",
-#         "about" :"Hello! I'm a full stack developer that loves python and javascript"
-#     }
-
-#     return response_body
-#read the data
+CORS(api)
 collection_data= pd.read_csv('clean_data.csv',header=0)
 
 @api.route('/profile',methods=["POST"])
 def submit_input_value():
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
     input_value = request.form['inputValue']
     # print(input_value)
-    index = collection_data[collection_data['address'].str.contains(input_value)].index.tolist()[0]
+    try:
+        index = collection_data[collection_data['address'].str.contains(input_value)].index.tolist()[0]
+    except:
+        return  jsonify({})
     day = collection_data['collectionday'][index]
     nextwaste = collection_data['nextwaste'][index]
     nextrecycle = collection_data['nextrecycle'][index]
@@ -29,3 +27,11 @@ def submit_input_value():
     response_test = {'response': test,'response1': test}
     # print(response_test)
     return jsonify(response_data)
+
+@api.route('/',methods=["GET"])
+def hello_world():
+    # print(request.args.get(name'))
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+    return "<p>Hello, World!</p>"
--- src/pages/Binday.js
+++ src/pages/Binday.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import '../App.css';
 
 export function Binday() {
@@ -17,26 +18,52 @@ export function Binday() {
     const handleSubmit = (event) => {
         event.preventDefault();
         if (inputValue !== "") {
-            //make query here
-            setCollectionDay("Monday");
-            setLandfill("2023-03-13");
-            setRecycle("2023-03-13");
-            setFood("2023-03-13");
+
+            // 
+            const formData = new FormData();
+            formData.append('inputValue', inputValue);
+
+            axios.post('http://127.0.0.1:5000/profile', formData)
+                .then(response => {
+
+                    if (JSON.stringify(response.data).length <5) {
+                        setCollectionDay("");
+                        setLandfill("");
+                        setRecycle("");
+                        setFood("");
+                        alert("wrong input")
+                    }
+                    else {
+                        const resjson = JSON.parse(response.data.replace("NaN", '""'));
+                        setCollectionDay(resjson.day ? resjson.day : "Not listed");
+                        setLandfill(resjson.landfilldate ? resjson.landfilldate : "Not listed");
+                        setRecycle(resjson.recycledate ? resjson.recycledate : "Not listed");
+                        setFood(resjson.fooddate ? resjson.fooddate : "Not listed");
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+            // //make query here
+            // setCollectionDay("Monday");
+            // setLandfill("2023-03-13");
+            // setRecycle("2023-03-13");
+            // setFood("2023-03-13");
 
         }
     };
 
     return (
-        <div style={{ color: "white" ,marginTop:"5vw"}}>
+        <div style={{ color: "white", marginTop: "5vw" }}>
             <form onSubmit={handleSubmit} style={{ display: "flex", justifyContent: "space-around" }}>
                 <label>
-                    Enter your postcode:
+                    Enter your address:
                 </label>
                 <input type="text" value={inputValue} onChange={handleInputChange} style={{ width: "20vw" }} />
 
                 <button type="submit">Submit</button>
             </form>
-            <div style={{marginTop:"5vw"}}>
+            <div style={{ marginTop: "5vw" }}>
                 <div className='dateinfo'>
                     <span>Collection Day:</span>
                     <span className='notifyDay'>{collectionDay}</span>
